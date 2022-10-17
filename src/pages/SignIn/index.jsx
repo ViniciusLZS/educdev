aa621b2fed21b91c89b5ba39794a827b0bb7312d
Vinicius <vinicius.luiz.9256@gmail.com>
2022-10-17 17:52:36 -0300

    login created

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Button from '../../components/Button';
+import Input from '../../components/Form/Input';
+import * as S from './styles';
 
 const SignIn = () => {
   return (
-    <div>
-      Login
-    </div>
-  )
-}
+    <S.Content>
+      <S.Title>Login</S.Title>
+      <S.Form>
+        <Input label="E-mail" type="email" name="email" placeholder='E-mail' />
+        <Input label='Senha' type='password' name='password' placeholder='Password' />
+        <Button>Entrar</Button>
+      </S.Form>
+    </S.Content>
+  );
+};
 
-export default SignIn
+export default SignIn;
