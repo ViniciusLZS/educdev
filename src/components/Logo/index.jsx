4f4ca9920b3fba7eaf63dc52284264a04bebaa55
Vinicius <vinicius.luiz.9256@gmail.com>
2022-10-17 17:50:28 -0300

    refactored link

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -6,11 +6,12 @@ import * as S from './styles';
 const Logo = () => {
   return (
     <S.BoxLogo>
-      <S.TitleH1>EducDev</S.TitleH1>
-      <S.Title>
-        Bem vindo <S.Span>ao</S.Span>
-      </S.Title>
       <Link to="/">
+        <S.TitleH1>EducDev</S.TitleH1>
+
+        <S.Title>
+          Bem vindo <S.Span>ao</S.Span>
+        </S.Title>
         <LogoSvg />
       </Link>
     </S.BoxLogo>
