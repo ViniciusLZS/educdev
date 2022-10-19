d9ed858c7b878caf0854c0bc0a9a0c93351cd81c
Vinicius <vinicius.luiz.9256@gmail.com>
2022-10-18 21:56:44 -0300

    change position and height

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -6,20 +6,28 @@ export const Content = styled.section`
   justify-content: center;
   flex-direction: column;
   width: 90%;
-  
 
   @media (min-width: 60rem) {
     border: 2px solid ${(props) => props.theme.grayPrimary};
     width: 42rem;
-    height: 36rem;
+    height: 38rem;
     padding: 1rem 4rem;
     border-radius: 10px;
   }
 `;
 
+export const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  max-width: 40rem;
+  height: 100%;
+`;
+
 export const Title = styled.h1`
   text-align: center;
-  margin-top: 2rem;
+  margin-top: 1rem;
   margin-bottom: 1rem;
   font-size: 3rem;
   font-weight: 500;
@@ -35,10 +43,4 @@ export const Title = styled.h1`
   text-shadow: -1px 0px 4px rgba(242, 159, 5, 0.25);
 `;
 
-export const Form = styled.form`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-`;
+
