e8675298de3c4248b9e901f7067f534bb9401207
Vinicius <vinicius.luiz.9256@gmail.com>
2022-10-19 15:45:29 -0300

    added eye icon and changed user case

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 import { ReactComponent as Usuario } from '../../assets/usuario-icon.svg';
 import { ReactComponent as Email } from '../../assets/email-icon.svg';
 import { ReactComponent as Senha } from '../../assets/lock-icon.svg';
+import { ReactComponent as ClosedEye } from '../../assets/closed-eye-icon.svg';
+import { ReactComponent as OpenEye } from '../../assets/open-eye-icon.svg';
+import * as S from './styles';
 
-
-const Icons = ({ label }) => {
-  console.log(typeof label);
+const Icons = ({ label, state }) => {
 
   switch (label) {
-    case 'Usuario':
+    case 'Usuário':
       return <Usuario />;
 
     case 'E-mail':
       return <Email />;
 
     case 'Senha':
-      return <Senha />;
+      return (
+        <>
+          <Senha />
+          {state ? (
+            <S.Icon>
+              <OpenEye />
+            </S.Icon>
+          ) : (
+            <S.Icon>
+              <ClosedEye />
+            </S.Icon>
+          )}
+        </>
+      );
     default:
   }
 };
