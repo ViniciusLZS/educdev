6d8e5afe7b930bca6179d503e441cc9161bee29f
Vinicius <vinicius.luiz.9256@gmail.com>
2022-10-22 19:06:36 -0300

    refactored props

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as S from './styles';
 
-const Button = ({props, children}) => {
+const Button = ({ children, ...props}) => {
   return <S.Button {...props}>{children}</S.Button>;
 };
 
