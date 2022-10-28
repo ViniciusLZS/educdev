d77d354fdf03b4d8b1caae7f9b71ad4c78e1a1e9
Vinicius <vinicius.luiz.9256@gmail.com>
2022-10-28 15:00:33 -0300

    added useLocation

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Logo from '../../components/Logo';
 import SignIn from '../SignIn';
 import SignUp from '../SignUp';
@@ -7,8 +7,10 @@ import SignInAndUp from '../SignInAndUp';
 import * as S from './styles';
 
 const Home = () => {
+  const { pathname } = useLocation();
+  
   return (
-    <S.Content>
+    <S.Content pathname={pathname}>
       <Logo />
       <Routes>
         <Route path="/" element={<SignInAndUp />} />
