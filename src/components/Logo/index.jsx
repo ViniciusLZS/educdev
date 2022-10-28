947ccf882e450da2f190501fb69e33fbaffe6f31
Vinicius <vinicius.luiz.9256@gmail.com>
2022-10-28 15:00:05 -0300

    added useLocation and modified title

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { ReactComponent as LogoSvg } from '../../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import * as S from './styles';
 
 const Logo = () => {
+  const { pathname } = useLocation();
   return (
     <S.BoxLogo>
       <Link to="/">
         <S.TitleH1>EducDev</S.TitleH1>
 
-        <S.Title>
-          Bem vindo <S.Span>ao</S.Span>
-        </S.Title>
+        {pathname.length === 1 ? (
+          <S.Title>
+            Bem vindo <S.Span>ao</S.Span>
+          </S.Title>
+        ) : (
+          ''
+        )}
+
         <LogoSvg />
       </Link>
     </S.BoxLogo>
